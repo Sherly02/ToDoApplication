7d77ddeb4f44601454627d656dc57db838b83fdd
Sherly <sherly_mauren_25rpl@student.smktelkom-mlg.sch.id>
2020-06-24 16:07:14 +0700

    adding to do list and add button

--- src/App.js
+++ src/App.js
@@ -1,28 +1,88 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component, useState} from "react";
+import axios from 'axios';
+import "./Todo.css";
 
-class App extends Component {
-  render() {
-    return (
-        <div className="App">
-          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
-            <p>
-              Edit <code>src/App.js</code> and save to reload.
-            </p>
-            <a
-                className="App-link"
-                href="https://reactjs.org"
-                target="_blank"
-                rel="noopener noreferrer"
-            >
-              Learn React
-            </a>
-          </header>
-        </div>
-    );
-  }
+class App extends Component{
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            isLoading: true,
+            todo: [],
+            error: null,
+        };
+
+        this.addItem = this.addItem.bind(this);
+    }
+
+
+    fetchData(){
+        const endpoint  = "https://virtserver.swaggerhub.com/hanabyan/todo/1.0.0/to-do-list";
+        fetch(endpoint)
+            .then(response => response.json())
+            .then(data =>
+                this.setState({
+                    todo: data,
+                    isLoading: false,
+                })
+            )
+            .catch(error => this.setState({ error, isLoading: false }));
+    }
+
+    componentDidMount() {
+       this.fetchData();
+    }
+
+    addItem(e) {
+        if (this._inputElement.value !== "") {
+            var newItem = {
+                title: this._inputElement.value,
+                createAt: Date.now()
+            };
+
+            this.setState((prevState) => {
+                return {
+                    items: prevState.items.concat(newItem)
+                };
+            });
+
+            this._inputElement.value = "";
+        }
+
+        console.log(this.state.items);
+
+        e.preventDefault();
+    }
+
+    render() {
+        const { isLoading, todo, error } = this.state;
+        return (
+            <React.Fragment>
+                <h1 align="center">To do List</h1>
+                {error ? <p>{error.message}</p> : null}
+                <div className="header">
+                    <form align="center" onSubmit={this.addItem}>
+                        <input ref={(a) => this._inputElement = a} placeholder="Add Task">
+                        </input>
+                        <button type="submit">+</button>
+                    </form>
+                </div>
+                <br />
+                {!isLoading ? (
+                    todo.map(todo => {
+                        const { id, title, description, status, createdAt } = todo;
+                        return (
+                            <div align="left" type="button" onClick={this.detail} className="button1">
+                                {title}
+                            </div>
+                        );
+                    })
+                ) : (
+                    <h3 align="center">Loading...</h3>
+                )}
+            </React.Fragment>
+        );
+    }
 }
 
 export default App;
